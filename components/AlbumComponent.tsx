f682e29a3fdf685c9d8e3435b66258d98dae17ed
ikeryanez <ikeryanez@gmail.com>
2022-12-23 16:26:17 +0100

    Individual album component styled

--- components/AlbumComponent.tsx
+++ components/AlbumComponent.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Text, View, Image } from 'react-native'
+import { Text, View, Image, StyleSheet } from 'react-native'
 import type { Album } from '../types'
 
 export type AlbumProps = {
@@ -8,11 +8,25 @@ export type AlbumProps = {
 
 const AlbumComponent = (props: AlbumProps) => {
   return (
-    <View>
-        <Image source = {{uri: props.album.coverUrl}} />
-        <Text> {props.album.artists} </Text>
+    <View style = {styles.container}>
+        <Image style = {styles.cover} source = {{uri: props.album.coverUrl}} />
+        <Text style = {styles.text}> {props.album.artists} </Text>
     </View>
   )
 }
 
 export default AlbumComponent
+
+const styles = StyleSheet.create({
+  container: {
+    width: 150,
+  },
+  cover:{
+    width: '100%',
+    height:150,
+    marginBottom: 5,
+  },
+  text:{
+    color: '#b3b3b3',
+  }
+});
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,12 +1,28 @@
 import React from 'react'
-import { View, Text } from '../components/Themed'
+import AlbumComponent from '../components/AlbumComponent'
+import { View, Text, StyleSheet } from 'react-native'
+import { Album } from '../types'
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style = {styles.container}>
+      <AlbumComponent album = {album} />
     </View>
   )
 }
 
+const album: Album = {
+  albumId: '0',
+  coverUrl: 'https://i.scdn.co/image/ab67706f00000003b1c15761720a3ba8d1e68228',
+  artists: 'Bad Bunny, J Balvin, Rauw Alejandro'
+}
+
 export default HomeScreen
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
